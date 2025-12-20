63cfa8189cb4c7c6f13f952aefe9d0fdb336b991
yash8855 <yash789778@gmail.com>
2025-12-21 03:32:13 +0530

    Value equal to index value

---
+++ basic/ValueEquaLindex .cpp
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to find elements in the array that are equal to their index.
+    vector<int> valueEqualToIndex(vector<int>& arr) {
+        vector<int> ans;
+        for(int i=0; i<arr.size();i++){
+            if(arr[i]==i+1){
+                ans.push_back(arr[i]);
+            }
+        }
+    return ans;   
+    }
+};
