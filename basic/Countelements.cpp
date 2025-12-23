b726ab5766f17c2baca318d0505ad2dd84b9a400
yash8855 <yash789778@gmail.com>
2025-12-23 19:55:14 +0530

    solved count smaller element

---
+++ basic/Countelements.cpp
@@ -0,0 +1,13 @@
+// Count of smaller elements
+class Solution {
+  public:
+    int countOfElements(int x, vector<int> &arr) {
+            int count=0;
+       for (int i=0; i<arr.size(); i++){
+           if(arr[i]<=x){
+               count++;
+           }
+       }
+    return count;  
+    }
+};
