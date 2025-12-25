10086d4f697d05bf028220a25a80a286b3039096
yash8855 <yash789778@gmail.com>
2025-12-26 00:02:52 +0530

    Two Sum - Pair with Given Sum

---
+++ basic/twoSum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target){
+        int start=0;
+        int end=arr.size()-1;
+        sort(arr.begin(),arr.end());
+        while(start<end){
+            int sum=arr[start]+arr[end];
+            if(sum==target){
+                return true;
+            }
+            else if(sum>target){
+                end--;
+            }else{
+                start++;
+            }
+        }
+    return false;    
+    }
+};
+
