d9e99d3777ed548ee3bf2066e85b40cb275db2cd
yash8855 <yash789778@gmail.com>
2025-12-28 22:45:24 +0530

    optimized Solution

--- basic/Maxproduct2numbers.cpp
+++ basic/Maxproduct2numbers.cpp
@@ -8,3 +8,23 @@ class Solution {
     return arr[n-1]*arr[n-2];
     }
 };  // Time complexity O(n log n)
+
+// otimized Solution
+class Solution {
+  public:
+        int maxProduct(vector<int>& arr) {
+        
+    int max1 = INT_MIN, max2 = INT_MIN;
+    
+        for (int x : arr) {
+            if (x > max1) {
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max2 = x;
+            }
+        }
+        return max1 * max2;
+    }
+};  // Time complexity O(n)
+
