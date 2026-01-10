cc22734b479cb1a9edab2504f9464a8af3a1f759
yash8855 <yash789778@gmail.com>
2026-01-10 14:47:02 +0530

    Missing no in array

--- 01_Arrays/Easy/01_missing_in_array.cpp
+++ 01_Arrays/Easy/01_missing_in_array.cpp
@@ -1,15 +1,15 @@
-class Solution {
-  public:
-    int missingNum(vector<int>& arr) {
-        long long n = arr.size() + 1;
-
-        long long total = (n * (n + 1LL)) / 2;  // force long long
-        long long sum = 0;
-
-        for(int i = 0; i < arr.size(); i++){
-            sum += arr[i];
-        }
-
-        return (int)(total - sum);
-    }
-};
+ class Solution {
+  public:  
+     int missingNum(vector<int>& arr) {
+         long long n = arr.size() + 1;
+  
+         long long total = (n * (n + 1LL)) / 2;  // force long long
+         long long sum = 0;
+ 
+         for(int i = 0; i < arr.size(); i++){
+             sum += arr[i];
+         }
+ 
+         return (int)(total - sum);
+     }
+ };
