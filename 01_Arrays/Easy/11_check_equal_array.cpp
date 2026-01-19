eb201119027737de68c69f84cbcae851d0275d9c
yash8855 <yash789778@gmail.com>
2026-01-19 21:40:35 +0530

    check equal Array

---
+++ 01_Arrays/Easy/11_check_equal_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+       sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       if(a==b){
+           return true;
+       }
+       else{
+           return false;
+       }
+    }
+};
