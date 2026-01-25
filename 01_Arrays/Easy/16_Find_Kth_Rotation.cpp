113496ff847270d242c382ad93663f15747d31f9
yash8855 <yash789778@gmail.com>
2026-01-25 23:50:47 +0530

    find kth rotations

---
+++ 01_Arrays/Easy/16_Find_Kth_Rotation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        int n = arr.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return i + 1;   // rotation point found
+            }
+        }
+
+        // array already sorted (no rotation)
+        return 0;
+    }
+};
