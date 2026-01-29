5bd69ce0395ada69de8eaa80db54b2516bbf6a52
yash8855 <yash789778@gmail.com>
2026-01-29 23:47:59 +0530

    find first 1 which index optimize approach

--- 01_Arrays/Easy/20_find_transition_point.cpp
+++ 01_Arrays/Easy/20_find_transition_point.cpp
@@ -13,3 +13,29 @@ class Solution {
     }
 };
 // Time Complexity: O(n)
+
+
+
+class Solution {
+  public:
+    int transitionPoint(vector<int>& arr) {
+        int start=0;
+        int end=arr.size()-1;
+        int ans=-1;
+        if(arr[0]==1){
+            return 0;
+        }
+        while(start<=end){
+            int mid =start+ (end-start)/2;
+            if(arr[mid]==1){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+    return ans;
+    }
+};
+// Time Complexity: O(log n)
